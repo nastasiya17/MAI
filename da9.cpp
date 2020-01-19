ad6d35beaccaf9e1a7abda4c085ffae5c27d6b04
nastasiya17 <59894138+nastasiya17@users.noreply.github.com>
2020-01-19 23:41:19 +0300

    Create da9.cpp

---
+++ da9.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+const long INF=1000000000000000000;
+
+int main() {
+	int n, m, start, finish;
+	cin >> n >> m >> start >> finish;
+	vector<long> v(n+1, INF);
+	vector <vector<long> > edges_list(m, vector<long> (3));
+	v[start]=0;
+
+	for (int i=0; i<m; i++) {
+		cin >> edges_list[i][0] >> edges_list[i][1] >> edges_list[i][2];
+	}
+
+	for (int k=1; k<n; k++){
+		bool move=false;
+		for (int i=0; i<m; i++) {
+			int st=edges_list[i][0];
+			int fin=edges_list[i][1];
+			int wei=edges_list[i][2];
+			if (v[st]<INF)
+				if (v[st] + wei < v[fin]) {
+					v[fin]=v[st] + wei;
+					move=true;
+				}
+		}
+		if (!move)
+			break;
+	}
+
+	if (v[finish]!=INF)
+		cout << v[finish] << endl;
+	else 
+		cout << "No solution" << endl;
+}
